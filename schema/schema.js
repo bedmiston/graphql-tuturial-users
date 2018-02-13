a4046fd070b47a500a37bb67482d70a201f8cdd0
Brendon Edmiston <Brendon_Edmiston@reyrey.com>
2018-02-13 06:54:06 -0600

    Initial commit

---
+++ schema/schema.js
@@ -0,0 +1,49 @@
+const graphql = require("graphql");
+const { GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLInt } = graphql;
+const axios = require("axios");
+
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString }
+  }
+});
+
+const UserType = new GraphQLObjectType({
+  name: "User",
+  fields: {
+    id: { type: GraphQLString },
+    firstName: { type: GraphQLString },
+    age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(parentValue, args) {
+        console.log(parentValue, args);
+        return axios
+          .get(`http://localhost:4001/companies/${parentValue.companyId}`)
+          .then(response => response.data);
+      }
+    }
+  }
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    user: {
+      type: UserType,
+      args: { id: { type: GraphQLString } },
+      resolve(parentValue, args) {
+        return axios
+          .get(`http://localhost:4001/users/${args.id}`)
+          .then(response => response.data);
+      }
+    }
+  }
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery
+});
